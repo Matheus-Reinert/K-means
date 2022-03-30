f0cc55cc023471dd0abcb0aec758e03c0419e23b
Matheus-Reinert <reinert.matheus@hotmail.com>
2022-03-29 21:20:10 -0300

    first commit

---
+++ kmeans.py
@@ -0,0 +1,16 @@
+import numpy  as np
+import pandas as pd
+import matplotlib.pyplot as pl
+import seaborn as sb
+from sklearn.cluster import KMeans
+
+
+df = pd.read_csv('iris.csv')
+X = np.array(df.drop('target',axis = 1))
+kmeans = KMeans(n_clusters=3, random_state=0)
+kmeans.fit(X)
+df['K-classes'] = kmeans.labels_
+#print(df)
+sb.pairplot(df,hue='target')
+
+
